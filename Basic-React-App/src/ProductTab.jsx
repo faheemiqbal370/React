6b95ecacef9e5a7b1e167b6abbf91ec1e8766dc8
Faheem Iqbal <zargarfaheem4229@gmail.com>
2025-12-15 15:47:00 +0530

    Rendering Arrays

--- Basic-React-App/src/ProductTab.jsx
+++ Basic-React-App/src/ProductTab.jsx
@@ -1,15 +1,15 @@
 import Product from "./product";
-
-function ProductTab(){
-    // let options = [<li>wow</li> , <li>cool</li>];
-    let options = ["wow" , "cool"];
-    // let options2 ={a: "HI-TECH",b: "DURABLE", c: "FAST" }
-    return(
-        <>
-        <Product title="Phone" price="30,000" features={options} />
-        {/* <Product title="LAptop" price={50000}  features={options} features2={options2}/> */}
-        {/* <Product title="Pen" price="80" features={options} features2={options2}/> */}
-        </>
-    );
-}
+//Rendering Arrays and Last ones
+// function ProductTab(){
+//     // let options = [<li>wow</li> , <li>cool</li>];
+//     let options = ["wow" , "cool"];
+//     // let options2 ={a: "HI-TECH",b: "DURABLE", c: "FAST" }
+//     return(
+//         <>
+//         <Product title="Phone" price="30,000" features={options} />
+//         {/* <Product title="LAptop" price={50000}  features={options} features2={options2}/> */}
+//         {/* <Product title="Pen" price="80" features={options} features2={options2}/> */}
+//         </>
+//     );
+// }
 export default ProductTab;
