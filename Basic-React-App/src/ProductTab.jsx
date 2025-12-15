d6086bc7cece99a1ca0dba37c4c764c29208a040
Faheem Iqbal <zargarfaheem4229@gmail.com>
2025-12-15 15:20:16 +0530

    Props

--- Basic-React-App/src/ProductTab.jsx
+++ Basic-React-App/src/ProductTab.jsx
@@ -1,11 +1,13 @@
 import Product from "./product";
 
 function ProductTab(){
+    let options = ["hi=tech", "Durable", "fast"];
+    let options2 ={a: "HI-TECH",b: "DURABLE", c: "FAST" }
     return(
         <>
-        <Product title="Phone" price="30,000"/>
-        <Product title="LAptop" price={50000}/>
-        <Product title="Pen" price="80"/>
+        <Product title="Phone" price="30,000" features={["wow","cow"]} features2={options2}/>
+        <Product title="LAptop" price={50000}  features={options} features2={options2}/>
+        <Product title="Pen" price="80" features={options} features2={options2}/>
         </>
     );
 }
--- Basic-React-App/src/product.jsx
+++ Basic-React-App/src/product.jsx
@@ -1,9 +1,12 @@
 import "./product.css";
-function Product({title,price}){
+function Product({title,price,features,features2}){
     return(
         <div className="product">
             <h3>{title}</h3>
             <h5>Price : {price}</h5>
+            <p>{features}</p>
+            <p>{features2.b}</p>
+            <p>{features2.c}</p>
         </div>
     );
 }
